ff721cefcbaf97e3e35c3cbadc2497ca5693dd66
Rahul Behera <thebehera@fb.com>
2021-11-02 12:40:57 -0700

    kick off new build

--- src/commonMain/kotlin/com/ditchoom/mqtt/Exception.kt
+++ src/commonMain/kotlin/com/ditchoom/mqtt/Exception.kt
@@ -7,6 +7,7 @@ import com.ditchoom.mqtt.controlpacket.format.ReasonCode
 open class MqttException(msg: String, val reasonCode: UByte) : Exception(msg)
 open class MalformedPacketException(msg: String) : MqttException(msg, 0x81.toUByte())
 open class ProtocolError(msg: String) : MqttException(msg, 0x82.toUByte())
+
 class MalformedInvalidVariableByteInteger(value: UInt) : MqttException(
     "Malformed Variable Byte Integer: This " +
             "property must be a number between 0 and %VARIABLE_BYTE_INT_MAX . Read value was: $value",
