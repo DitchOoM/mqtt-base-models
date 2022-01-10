3fadf89c45a6beb4e42723d1f6a0a4fbc984f878
Rahul Behera <thebehera@fb.com>
2022-01-09 17:25:24 -0800

    rollback filter.kt changes

--- src/commonMain/kotlin/com/ditchoom/mqtt/topic/Filter.kt
+++ src/commonMain/kotlin/com/ditchoom/mqtt/topic/Filter.kt
@@ -7,8 +7,7 @@ import com.ditchoom.mqtt.base.Parcelize
 import com.ditchoom.mqtt.controlpacket.MqttUtf8String
 
 @Parcelize
-class Filter(private val topicFilter: String) : Parcelable {
-    constructor(filter: CharSequence): this(filter.toString())
+class Filter(val topicFilter: CharSequence) : Parcelable {
     fun validate(asServer: Boolean = false): Node? {
         return try {
             val rootNode = Node.from(MqttUtf8String(topicFilter))
